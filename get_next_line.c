77404ef578d8ccf2c833577e5397fa90c32020fc
Chataya Cummings <ccumming@e1r7p2.21-school.ru>
2019-01-16 13:30:28 +0300

    work

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ccumming <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 19:10:24 by ccumming          #+#    #+#             */
-/*   Updated: 2019/01/15 19:12:49 by ccumming         ###   ########.fr       */
+/*   Updated: 2019/01/16 13:29:27 by ccumming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,22 @@
 
 int			get_next_line(const int fd, char **line)
 {
+	t_list		list;
+	int			ret;
+	char		buff[BUFF_SIZE];
+
 	if (fd < 0)
 	{
 		ft_putstr("Can't open file...");
 		return (-1);
 	}
 	
-	ft_putstr("start our work now...\n");
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	{
 
+		ft_putstr(buff);
+		ft_strclr(buff);
+	}
 
 	// 1 - line has been read, 0 - file has been read, -1 - error
 	return (0);
